ea317f8818fe4f547e5ad864a045169672e530c5
abed <abed472011@gmail.com>
2020-03-06 17:31:19 +0200

    added connection to db

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+
+dotenv.config();
+
+mongoose.connect(
+    process.env.DB_CONNECT,
+{ useUnifiedTopology: true }
+,() => {
+    console.log('connected to db');
+})
 
 /**
  * @import routes
