7f1d33a0095a55ef65ff0009315a7166ef02e561
abed <abed472011@gmail.com>
2020-03-06 17:07:00 +0200

    created register route, added middleware

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 const express = require('express');
 const app = express();
 
+/**
+ * @import routes
+ */
+const authRoute = require('./routes/auth');
+
+/**
+ * creating router middleware
+ */
+app.use('/api/user',authRoute);
+
 app.listen(3000,() => console.log('server started'));
