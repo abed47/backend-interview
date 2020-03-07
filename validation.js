86a0ade31ed26b8e290c087c3d73f66fe6f019e0
abed <abed472011@gmail.com>
2020-03-07 17:28:05 +0200

    seperated validation code

---
+++ validation.js
@@ -0,0 +1,14 @@
+const joy = require('@hapi/joi');
+
+const registerValidation = async (data) => {
+    const validationSchema = joy.object({
+        fullname : joy.string().max(100).min(6).required(),
+        user_name : joy.string().max(100).min(6).required(),
+        password: joy.string().max(60).min(6).required(),
+        date: joy.date(),
+    });
+    
+    return await validationSchema.validate(data);
+}
+
+module.exports = registerValidation;
