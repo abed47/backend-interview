505a461bffc157940733781d17080ccdd901a5c8
abed <abed472011@gmail.com>
2020-03-09 01:49:15 +0200

    added test for routes

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const dotenv = require('dotenv');
 
 //Import Routes
 const auth = require('./routes/auth');
-const postsRoute = require('./routes/post');
 
 //dot env init
 dotenv.config();
@@ -25,9 +24,6 @@ const authRoutes = new auth(express);
 //adding json to express
 app.use(express.json());
 
-//middilewre for post routes
-app.use('/api/posts',postsRoute);
-
 /**
  * creates middleware for the auth route
  * 
@@ -39,6 +35,4 @@ app.use('/api/posts',postsRoute);
 app.use('/api/user',authRoutes.initRoutes())
 
 
-app.listen(3000, () => {
-    console.log('server started');
-})
+module.exports = app.listen(3000, () => console.log('server started'))
---
+++ test/index.js
@@ -0,0 +1,61 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('../index');
+const chaiJwt = require('chai-jwt');
+
+chai.use(chaiHttp)
+chai.use(chaiJwt)
+const expect = chai.expect;
+/*
+describe('POST /api/user/register', () => {
+    it('it should register a new user', done => {
+        chai.request(app).post('/api/user/register')
+        .send({fullname: "test full name",user_name: "testusamedsd155",password:"testpassword",date: "1583704927"})
+        .then(res => {
+            const body = res.body;
+            expect(body).to.contain.property('_id');
+            expect(body).to.contain.property('date');
+            expect(body).to.contain.property('fullname');
+            expect(body).to.contain.property('user_name');
+            expect(body).to.contain.property('password');
+            expect(body).to.contain.property('__v');
+            done();
+        })
+        .catch(err => {
+            done(err);
+        })
+    });
+});
+*/
+describe('POST /api/user/login',() => {
+    it('it should login and return auth token', done => {
+        chai.request(app)
+        .post('/api/user/login')
+        .send({user_name: "abed1996",password: "123456"})
+        .then(res => {
+            const body = res.text;
+            const headers = res.header;
+            expect(body).to.contain.string('logged in')
+            expect(headers).to.contain.property('auth-token')
+            expect(headers['auth-token']).to.be.a.jwt
+            done();
+        }).catch(err => {
+            done(err);
+        })
+    })
+})
+
+describe('POST /api/user/resize',() => {
+    it('it should return an image attachment', () => {
+        chai.request(app)
+        .post('/api/user/resize')
+        .send({imgUrl: "https://miro.medium.com/max/2400/1*LSYdlwkdqEQSA-p19HGF7Q.jpeg"})
+        .then(res => {
+            expect(res).to.have.status(200);
+            expect(res.header['Content-Type']).to.be.eql('image/png')
+            done()
+        }).catch(err => {
+            done(err)
+        })
+    })
+})
